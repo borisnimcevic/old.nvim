21b1e46edfaa029ea999ec44c2afc7b1e1949d55
Boris Nimcevic <borisnimcevic@gmail.com>
2021-11-03 13:29:41 +0100

    Initial minimal setup.

---
+++ init.vim
@@ -0,0 +1,9 @@
+call plug#begin('~/.vim/plugged')
+
+" Parsing
+Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
+
+" Colors
+Plug 'navarasu/onedark.nvim'
+
+call plug#end()
---
+++ plugin/sets.lua
@@ -0,0 +1,14 @@
+vim.g.mapleader = ' '
+vim.api.nvim_set_keymap('n', '<leader>b', ':buffers<CR>:buffer ', {noremap = true}) 
+
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = "$HOME/.vim/undodir"
+vim.opt.undofile = true
+
+-- better serach 
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
