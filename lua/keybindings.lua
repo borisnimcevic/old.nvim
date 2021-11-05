4e1729fdc16110c8ded29acda22ccfc729badd36
Boris Nimcevic <borisnimcevic@gmail.com>
2021-11-05 20:34:45 +0100

    Updated keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -4,5 +4,11 @@ local map = vim.api.nvim_set_keymap
 
 -- Buffers
 map('n', '<leader>b', ':buffers<CR>:buffer ', {noremap = true}) 
-map('n', '<S-h>', ':bprev<CR>', {noremap = true}) 
-map('n', '<S-l>', ':bnext<CR>', {noremap = true}) 
+-- map('n', '<S-h>', ':bprev<CR>', {noremap = true}) 
+-- map('n', '<S-l>', ':bnext<CR>', {noremap = true}) 
+map('n', 'H', ':bprev<CR>', {noremap = true}) 
+map('n', 'L', ':bnext<CR>', {noremap = true}) 
+
+-- General
+map('n', 'J', 'j', {noremap = true}) 
+map('v', 'J', 'j', {noremap = true}) 
