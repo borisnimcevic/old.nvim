337794ce55e8b18d01103059a476e424e4ffbefd
Boris Nimcevic <borisnimcevic@gmail.com>
2021-11-05 01:46:35 +0100

    Added git commentary

--- init.lua
+++ init.lua
@@ -1,7 +1,17 @@
 local Plug = vim.fn['plug#']
 vim.call('plug#begin', '~/.vim/plugged')
+
 -- Parsing
 Plug ('nvim-treesitter/nvim-treesitter', {['do'] = 'TSUpdate'})
+
 -- Colors
 Plug 'navarasu/onedark.nvim'
+
+-- Commenting
+Plug 'terrortylor/nvim-comment'
+
 vim.call('plug#end')
+
+require('nvim_comment').setup({
+  comment_empty = false,
+})
