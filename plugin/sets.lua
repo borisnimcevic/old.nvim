77422d2d0424172aab54d7104d3455887dd44bfe
Boris Nimcevic <borisnimcevic@gmail.com>
2021-11-05 01:22:48 +0100

    Fixed indentation.

--- plugin/sets.lua
+++ plugin/sets.lua
@@ -1,14 +1,24 @@
+-- general
 vim.opt.number = true
---vim.opt.relativenumber = true
+vim.opt.list = true
+vim.opt.listchars = {tab = '▸ ', trail = '·'}
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+vim.opt.title = true
 
+-- backup files
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.cmd('set undodir=~/.vim/undodir')
+vim.cmd('set undodir=~/.vim/undodir') -- this doesn't work if it is written in lua
 vim.opt.undofile = true
 
 -- better serach 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.list = true
-vim.opt.listchars = {tab = '▸ ', trail = '·'}
+-- tabs and indentation
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -1,1 +1,1 @@
-require'nvim-treesitter.configs'.setup { indent = { enable = true }, highlight = { enable = true }, incremental_selection = { enable = true }, textobjects = { enable = true }}
+require'nvim-treesitter.configs'.setup { indent = { enable = false }, highlight = { enable = true }, incremental_selection = { enable = true }, textobjects = { enable = true }}
